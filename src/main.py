850dcec90b0b5c60accc6e8e01fbddf4b70cc85d
AlejandroLabourdette <jal.labourdette@gmail.com>
2023-06-30 02:40:21 -0400

    urls can be stored in a file to be used from

--- src/main.py
+++ src/main.py
@@ -12,32 +12,41 @@ from utils import was_already_downloaded
 @click.version_option(version='0.1.0')
 @click.argument('urls', nargs=-1)
 @click.option('--onlyvideos', is_flag=True, help='download only videos')
-def download(urls, onlyvideos):
+@click.option('--use_urls_file', is_flag=True, help='download specified urls in "urls" file')
+def download(urls, onlyvideos, use_urls_file):
     '''Download full directory from specified URL's'''
     url: str
+    if use_urls_file:
+        with open('urls') as file_urls:
+            for url in file_urls:
+                url = url.removesuffix('\n')
+                if url == '':
+                    continue
+                download_url(url, onlyvideos)
     for url in urls:
-        click.echo(f'Downloading from url: {url}')
-        dir_name = url.split('/')[-2] + '/'
-        click.echo(f'Saving under dir: {dir_name}')
-        links = get_links(url, onlyvideos, dir_to_save=dir_name)
-        for link_data in links:
-            url_to_file = link_data[0]
-            file_name = link_data[1]
-            dir_to_save = 'downloads/' + link_data[2]
-            relative_path = dir_to_save + file_name
+        download_url(url, onlyvideos)
 
-            if not os.path.isdir(dir_to_save):
-                os.makedirs(dir_to_save)
+def download_url(url, onlyvideos):
+    click.echo(f'Downloading from url: {url}')
+    dir_name = url.split('/')[-2] + '/'
+    click.echo(f'Saving under dir: {dir_name}')
+    links = get_links(url, onlyvideos, dir_to_save=dir_name)
+    for link_data in links:
+        url_to_file = link_data[0]
+        file_name = link_data[1]
+        dir_to_save = 'downloads/' + link_data[2]
+        relative_path = dir_to_save + file_name
 
-            click.echo(f'dwnlding: {relative_path}')
-            if not was_already_downloaded(url_to_file, relative_path):
-                urllib.request.urlretrieve(
-                    url_to_file, relative_path, 
-                    DownloadProgressBar())
-            else:
-                click.echo('Already downloaded')
+        if not os.path.isdir(dir_to_save):
+            os.makedirs(dir_to_save)
 
-
+        click.echo(f'dwnlding: {relative_path}')
+        if not was_already_downloaded(url_to_file, relative_path):
+            urllib.request.urlretrieve(
+                url_to_file, relative_path, 
+                DownloadProgressBar())
+        else:
+            click.echo('Already downloaded')
 
 if __name__ == '__main__':
     download()
